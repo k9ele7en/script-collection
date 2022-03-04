7d980f1f2d47f3e80b3e980581f4c56c92563f7f
Officer K <53246028+k9ele7en@users.noreply.github.com>
2022-03-04 15:51:31 +0700

    Add remove <none> tagged images

--- docker/remove_local.sh
+++ docker/remove_local.sh
@@ -1,2 +1,10 @@
 # filter by image name, tag diferrent than grep -v, ex: latest
 sudo docker rmi $(sudo docker images -q --filter=reference='image-name' |grep -v 'latest')
+
+#According to the docker documentation you can list only untagged (dangling) images with
+
+docker images -f "dangling=true"
+#and redirect them to docker rmi command like that:
+
+docker rmi $(docker images -f "dangling=true" -q) --force
+#Notice -q param thats only show numeric IDs of containers.
