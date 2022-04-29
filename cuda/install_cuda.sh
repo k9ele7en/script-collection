677646a9a14048627ccb3dd0cf1e638e0a9efd06
k9eleven <huukim98@gmail.com>
2022-04-29 16:00:20 +0700

    add debian 10 cuda ver

--- cuda/install_cuda.sh
+++ cuda/install_cuda.sh
@@ -10,6 +10,11 @@ sudo apt update
 sudo apt install build-essential
 
 ## install cuda 
+
+## Debian 10
+wget https://developer.download.nvidia.com/compute/cuda/11.4.3/local_installers/cuda_11.4.3_470.82.01_linux.run
+sudo sh cuda_11.4.3_470.82.01_linux.run
+
 ## Debian 11, Ubuntu
 wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run
 sudo sh cuda_11.6.2_510.47.03_linux.run
