f0074090e57d845251ffd2c986a802e32c819353
k9eleven <huukim98@gmail.com>
2022-04-30 15:47:03 +0700

    install cuda from apt

--- cuda/install_cuda.sh
+++ cuda/install_cuda.sh
@@ -1,3 +1,4 @@
+#### Prepare
 ##verify gpu-device
 lspci | grep -i nvidia
 ##verify linux OS
@@ -10,7 +11,7 @@ sudo apt update
 sudo apt install build-essential
 
 ## install cuda 
-
+###By run file
 ## Debian 10
 wget https://developer.download.nvidia.com/compute/cuda/11.4.3/local_installers/cuda_11.4.3_470.82.01_linux.run
 sudo sh cuda_11.4.3_470.82.01_linux.run
@@ -18,3 +19,13 @@ sudo sh cuda_11.4.3_470.82.01_linux.run
 ## Debian 11, Ubuntu
 wget https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda_11.6.2_510.47.03_linux.run
 sudo sh cuda_11.6.2_510.47.03_linux.run
+
+
+###By apt file
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/ /"
+sudo add-apt-repository contrib
+
+##if update fail bc key not found then https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
+sudo apt-get update
+sudo apt-get -y install cuda
